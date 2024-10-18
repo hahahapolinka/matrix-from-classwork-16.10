0997dbea7ddfb2b3deccee04328b1e713fe10ee7
hahahapolinka <gavrilova.pv@edu.spbstu.ru>
2024-10-18 15:08:42 +0300

    Cjrrection of defects

--- main.cpp
+++ main.cpp
@@ -5,12 +5,13 @@ int main()
   size_t m = 0;
   size_t n = 0;
   std::cin >> m >> n;
+  int** t = nullptr;
   if (!std::cin) {
         std::cout << "Wrong input!\n";
         return 1;
   }
   try {
-	int** t = matrix(m, n);
+	t = matrix(m, n);
   }catch(const std::bad_alloc & e){
 	std::cerr << "Out of memory!\n";
 	return 1;
