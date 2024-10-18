24b6171774df926683c59c95b85b3ce65a93851f
hahahapolinka <gavrilova.pv@edu.spbstu.ru>
2024-10-18 14:58:38 +0300

    Add exceptoin handing

--- main.cpp
+++ main.cpp
@@ -9,7 +9,12 @@ int main()
         std::cout << "Wrong input!\n";
         return 1;
   }
-  int** t = matrix(m, n);
+  try {
+	int** t = matrix(m, n);
+  }catch(const std::bad_alloc & e){
+	std::cerr << "Out of memory!\n";
+	return 1;
+  }
   enter(t, m, n);
   out(t, m, n);
   std::cout << '\n';
