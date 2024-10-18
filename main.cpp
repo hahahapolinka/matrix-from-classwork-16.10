8be806522f0cc6c805f111bff0078746235c6623
hahahapolinka <gavrilova.pv@edu.spbstu.ru>
2024-10-18 14:48:49 +0300

    Add input check

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
-#include <iostream>
 #include "matrix.hpp"
+#include <iostream>
 int main()
 {
   size_t m = 0;
   size_t n = 0;
-  int** t = matrix(m, n);
   std::cin >> m >> n;
-  std::cout << m << " " << n;
+  if (!std::cin) {
+        std::cout << "Wrong input!\n";
+        return 1;
+  }
+  int** t = matrix(m, n);
+  enter(t, m, n);
+  out(t, m, n);
   std::cout << '\n';
   clear(t, m);
 }
